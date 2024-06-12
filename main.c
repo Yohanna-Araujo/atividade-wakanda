02f4782eaef39b2065943127fdab5ec81873b575
Yohanna Araújo <143648634+Yohanna-Araujo@users.noreply.github.com>
2024-06-11 21:27:25 -0300

    main.c

--- main.c
+++ main.c
@@ -5,20 +5,20 @@
 int main() {
     const char *nomeArquivo = "teste01.txt";
 
-    double menor_vizinhanca = calcularMenorVizinhanca(nomeArquivo);
-    if (menor_vizinhanca < 0) {
-        printf("Erro ao calcular a menor vizinhanca\n");
+    // Calcular e imprimir a menor vizinhança
+    double menorVizinhanca = calcularMenorVizinhanca(nomeArquivo);
+    if (menorVizinhanca < 0) {
+        printf("Erro ao calcular a menor vizinhanca.\n");
         return 1;
     }
-    printf("Menor vizinhanca: %.2f\n", menor_vizinhanca);
+    printf("Menor vizinhanca: %.2f\n", menorVizinhanca);
 
-    char *cidade_menor_vizinhanca = cidadeMenorVizinhanca(nomeArquivo);
-    if (cidade_menor_vizinhanca == NULL) {
-        printf("Erro ao encontrar a cidade com a menor vizinhanca\n");
+    // Obter e imprimir a cidade com a menor vizinhança
+    char *cidade = cidadeMenorVizinhanca(nomeArquivo);
+    if (!cidade) {
+        printf("Erro ao obter a cidade com a menor vizinhanca.\n");
         return 1;
     }
-    printf("Cidade com a menor vizinhanca: %s\n", cidade_menor_vizinhanca);
-    free(cidade_menor_vizinhanca);
 
     return 0;
 }
